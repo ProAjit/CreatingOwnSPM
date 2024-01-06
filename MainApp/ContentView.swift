91c023f4b474d5d46557eadea37940d812c636f9
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-01-07 02:32:05 +0530

    Completed MainApp using FindMyIP library

--- MainApp/ContentView.swift
+++ MainApp/ContentView.swift
@@ -6,17 +6,46 @@
 //
 
 import SwiftUI
+import FindMyIPAjitS
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @StateObject public var viewModel: FindMyIPViewModel = FindMyIPViewModel()
+    
+    public var body: some View {
+        NavigationView {
+            VStack {
+                Spacer()
+                if viewModel.isLoading {
+                    ProgressView("Fetching Data...")
+                } else {
+                    if let findMyIPData = viewModel.findMyIPData {
+                        Group {
+                            Text("My IP \(findMyIPData.ip)")
+                                .font(.footnote)
+                            Spacer()
+                            Text("My Network \(findMyIPData.network)")
+                                .font(.footnote)
+                            Spacer()
+                        }
+                        Spacer()
+                    } else {
+                        Text("Failed to fetch data.")
+                            .foregroundColor(.red)
+                    }
+                }
+                Spacer()
+            }
+            .navigationTitle("Find My IP")
+            .alert(isPresented: $viewModel.showAlert) {
+                Alert(title: Text("Error"), message: Text(viewModel.findMyIPError), dismissButton: .default(Text("OK")))
+            }
         }
-        .padding()
+        .onAppear {
+            viewModel.getFindMyIPServiceList()
+        }
     }
+    
 }
 
 #Preview {

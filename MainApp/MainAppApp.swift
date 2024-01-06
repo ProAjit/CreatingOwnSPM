29fac7d52ded3ef9f7f11bb1f7c38699729500e1
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-01-06 19:40:43 +0530

    Initial Commit

---
+++ MainApp/MainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainAppApp.swift
+//  MainApp
+//
+//  Created by Ajit Satarkar on 06/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
